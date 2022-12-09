82e2c85cc49c759ff8e73ec8f7625de6ae11439f
Valentin271 <valentin.doreau@free.fr>
2022-12-09 18:56:00 +0100

    feat: première ébauche page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-import {
-    EuiLink,
-    EuiCode,
-} from '@elastic/eui';
+import { EuiProvider } from '@elastic/eui';
+import '@elastic/eui/dist/eui_theme_light.css';
+
+import { Create } from './views';
 
 const App = () => (
-    <div className="App">
-        <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-            Edit <EuiCode transparentBackground>src/App.tsx</EuiCode> and save to reload.
-            </p>
-            <EuiLink href="https://reactjs.org" target="_blank">
-            Learn React
-            </EuiLink>
-        </header>
-    </div>
+    <EuiProvider colorMode="light">
+        <Create />
+    </EuiProvider>
 );
 
 export default App;
---
+++ src/views/Create.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import {
+    EuiPage,
+    EuiPageBody,
+    EuiPageSection,
+    EuiEmptyPrompt,
+    EuiButton,
+    EuiResizableContainer,
+    EuiLoadingContent,
+    EuiLoadingChart,
+    EuiPanel,
+} from '@elastic/eui';
+
+const Create = () => {
+    const [empty, setEmpty] = React.useState(true);
+
+    return (
+        <EuiPage grow>
+            <EuiPageBody>
+                {empty ? (
+                    <EuiPageSection
+                        color="transparent"
+                        alignment="center"
+                        grow
+                    >
+                        <EuiEmptyPrompt
+                            title={<span>Aucune application</span>}
+                            color="plain"
+                            iconType="graphApp"
+                            iconColor="default"
+                            actions={<EuiButton fill onClick={() => {setEmpty(!empty);}}>Créer une application</EuiButton>}
+                        />
+                    </EuiPageSection>
+                ) : (
+                    <EuiPageSection color="plain" alignment="top" grow>
+                        <EuiResizableContainer>
+                            {(EuiResizablePanel, EuiResizableButton) => (
+                                <>
+                                    <EuiResizablePanel mode="main" initialSize={70} minSize="40%">
+                                        <EuiLoadingChart size="xl" />
+                                    </EuiResizablePanel>
+                                    <EuiResizableButton />
+                                    <EuiResizablePanel
+                                        mode="collapsible"
+                                        initialSize={30}
+                                        minSize="20%"
+                                    >
+                                        <EuiPanel grow>
+                                            <EuiLoadingContent lines={10} />
+                                        </EuiPanel>
+                                    </EuiResizablePanel>
+                                </>
+                            )}
+                        </EuiResizableContainer>
+                    </EuiPageSection>
+                )}
+            </EuiPageBody>
+        </EuiPage>
+    );
+};
+
+export default Create;
